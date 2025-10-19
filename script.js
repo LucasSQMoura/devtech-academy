63e1c16336554265fec97f9224520ee8a8324dd5
LucasSQMoura <lucas.moura0404@gmail.com>
2025-10-19 16:26:48 -0300

    Uso de classes com atributos, construtor e métodos

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+class Aluno {
+  constructor(nome, idade, curso, notaFinal) {
+    this.nome = nome;
+    this.idade = idade;
+    this.curso = curso;
+    this.notaFinal = parseFloat(notaFinal);
+  }
+
+  isAprovado() {
+    return this.notaFinal >= 7;
+  }
+
+  toString() {
+    return `${this.nome} (${this.curso}) - Nota: ${this.notaFinal}`;
+  }
+}
+
 let alunos = [];
 let editIndex = -1;
-
-const form = document.getElementById("formAluno");
-const tabela = document.getElementById("tabelaAlunos");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,47 +25,12 @@ form.addEventListener("submit", (e) => {
   const curso = document.getElementById("curso").value;
   const notaFinal = document.getElementById("notaFinal").value;
 
-  const aluno = { nome, idade, curso, notaFinal };
+  const aluno = new Aluno(nome, idade, curso, notaFinal);
 
-  if (editIndex === -1) {
-    alunos.push(aluno);
-  } else {
-    alunos[editIndex] = aluno;
-    editIndex = -1;
-  }
+  if (editIndex === -1) alunos.push(aluno);
+  else alunos[editIndex] = aluno;
 
+  editIndex = -1;
   form.reset();
   renderTabela();
 });
-
-function renderTabela() {
-  tabela.innerHTML = "";
-  alunos.forEach((aluno, index) => {
-    tabela.innerHTML += `
-      <tr>
-        <td>${aluno.nome}</td>
-        <td>${aluno.idade}</td>
-        <td>${aluno.curso}</td>
-        <td>${aluno.notaFinal}</td>
-        <td>
-          <button onclick="editarAluno(${index})">Editar</button>
-          <button onclick="excluirAluno(${index})">Excluir</button>
-        </td>
-      </tr>
-    `;
-  });
-}
-
-function editarAluno(index) {
-  const aluno = alunos[index];
-  document.getElementById("nome").value = aluno.nome;
-  document.getElementById("idade").value = aluno.idade;
-  document.getElementById("curso").value = aluno.curso;
-  document.getElementById("notaFinal").value = aluno.notaFinal;
-  editIndex = index;
-}
-
-function excluirAluno(index) {
-  alunos.splice(index, 1);
-  renderTabela();
-}
